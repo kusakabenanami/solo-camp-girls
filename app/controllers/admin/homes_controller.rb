143cd990b458dce9323898f8d31ccabdad033472
kusakabenanami <138275920+kusakabenanami@users.noreply.github.com>
2023-09-15 13:37:36 +0000

    [Add]solo-camp-girls

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,4 +1,19 @@
 class Admin::HomesController < ApplicationController
   def top
   end
+  
+  def top
+     @orders = Order.page(params[:page]).order(created_at: "DESC")
+     @quantity = 0
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:name, :address, :billing_amount, :post_number, :postage)
+  end
+
+  def address_params
+    params.require(:order).permit(:name, :address)
+  end
 end
