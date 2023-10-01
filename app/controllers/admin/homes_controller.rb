b79b521eac3ae7181b8d9646714c9eeff42a61b6
kusakabenanami <138275920+kusakabenanami@users.noreply.github.com>
2023-10-01 11:18:07 +0000

    [Add]solo_camp_girls

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -2,9 +2,18 @@ class Admin::HomesController < ApplicationController
 
   def top
     @users = User.all
-    @camp_comments = @users.map do |user|
-      user.camp_comments
-    end.flatten
-  end
+    search_term = params[:search].to_s.strip # .to_s で nil を空文字列に変換し、.strip で前後の空白を削除
 
+    if search_term.blank?
+      # 検索条件が空の場合はすべてのコメントを表示
+      @camp_comments = @users.map do |user|
+        user.camp_comments
+      end.flatten
+    else
+      # 検索条件が存在する場合は部分一致で検索
+      @camp_comments = @users.map do |user|
+        user.camp_comments.where("comment LIKE ?", "%#{search_term}%")
+      end.compact.flatten
+    end
+  end
 end
